da3d704863d5bf2cb669cf4400e047e65fe953c1
Olaf <oso@osoz.ch>
2021-01-30 16:28:10 +0100

    Upload initial version to repo

---
+++ start-pi-touchscreen-controller.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+# Original by Jon Eskdale edited and modified by Olaf Sonderegger 2021-01-01
+# Run program to start slide (picture slideshow), dim or turn off RPi backlight after a period of time,
+# turn on and stop slide when the touchscreen is touched.
+# Best to run this script from /etc/rc.local to start at boot.
+
+#EDIT THIS VALUE to set the period before slide will start
+slide_timeout=120 # seconds (2min)
+
+#EDIT THIS VALUE to set the path, where slide find pictures
+slide_pictures=/mnt/nas_medien/trips
+
+#EDIT THIS VALUE to set the period before it will dim
+dimmer_timeout=1800 # seconds (30min)
+
+#EDIT THIS VALUE to set the brightness it dims the display to.
+#50-253, minimum value is 50, blanking the screen will be done after blank timeout
+min_brightness=100
+
+#EDIT THIS VALUE to set the period before it will blank the screen
+blank_timeout=12600 # seconds (4h since slide_timeout)
+
+# Find the device the touchscreen uses.  This can change depending on
+# other input devices (keyboard, mouse) are connected at boot time.
+# for line in $(lsinput); do
+#        if [[ $line == *"/dev/input"* ]]; then
+#                word=$(echo $line | tr "/" "\n")
+#                for dev in $word; do
+#                        if [[ $dev == "event"* ]]; then
+#                                break
+#                       fi
+#              done
+#        fi
+#        if [[ $line == *"FT5406"* ]] ; then
+#               break
+#        fi
+#done
+
+pi-touchscreen-controller $slide_timeout $slide_pictures $dimmer_timeout $min_brightness $blank_timeout event0
